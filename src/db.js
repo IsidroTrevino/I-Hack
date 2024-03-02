21b894c521d325d46b3ae8c242592acf9a117ede
IsidroTrevino <A00837104@tec.mx>
2024-03-02 14:18:46 -0600

    Started frontend + Implemented primary design for login screen (no logic yet)

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise';
-
-export const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Itrevino1212',
-    port: 3306,
-    database: 'qualtia',
-});
