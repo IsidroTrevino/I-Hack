49dda964cdd6b9d8bde63fc0693613ffce2440e8
IsidroTrevino <A00837104@tec.mx>
2024-03-03 05:17:11 -0600

    More changes

--- frontend/Qualtia-App/src/screens/DairyScreen/DairyScreen.js
+++ frontend/Qualtia-App/src/screens/DairyScreen/DairyScreen.js
@@ -1,11 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View , ScrollView} from 'react-native'
 import Header from '../../components/Header'
+import Card from '../../components/Card/Card'
+import Queso from '../../../assets/images/panela.jpeg'
+import QM from '../../../assets/images/verdaderomancheg.jpeg'
 import React from 'react'
 
 const DairyScreen = () => {
   return (
     <View style={styles.root}>
       <Header leftIcon={'arrow-left'}/>
+      <ScrollView>
+      <Card 
+            text='Queso Panela' 
+            img={Queso} 
+            description={'El queso panela es un tipo de queso fresco que es originario de México y se utiliza ampliamente en la cocina tradicional mexicana.'}
+            price={'$100.75'}
+            Touchable = {false}
+        />
+
+        <Card 
+            text='Queso Manchego' 
+            img={QM} 
+            description={'El queso manchego es un queso español tradicional que se elabora principalmente con leche de oveja de la raza manchega. Este queso tiene una Denominación de Origen Protegida (DOP), lo que significa que debe cumplir con ciertos estándares y procedimientos de producción específicos para llevar la denominación "Manchego".'}
+            price={'$130.42'}
+            Touchable = {false}
+        />
+
+        <Card 
+            text='Queso Manchego' 
+            img={QM} 
+            description={'El queso manchego es un queso español tradicional que se elabora principalmente con leche de oveja de la raza manchega. Este queso tiene una Denominación de Origen Protegida (DOP), lo que significa que debe cumplir con ciertos estándares y procedimientos de producción específicos para llevar la denominación "Manchego".'}
+            price={'$130.42'}
+            Touchable = {false}
+        />
+
+      </ScrollView>
     </View>
   )
 }
