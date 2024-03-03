5af8ab9fbfe29d58a315e0eaf4100a6850fb7e50
IsidroTrevino <A00837104@tec.mx>
2024-03-03 11:26:18 -0600

    added mascot to loading and better functionality to MenuScreen

--- frontend/Qualtia-App/src/screens/MenuScreen/MenuScreen.js
+++ frontend/Qualtia-App/src/screens/MenuScreen/MenuScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import MenuOption from '../../components/MenuOptions/MenuOption';
 import Separator from '../../components/Separator';
@@ -6,38 +6,38 @@ import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 
 const MenuScreen = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const onPressArrow = () => {
-        navigation.goBack();
-    }
+  const onPressArrow = () => {
+    navigation.goBack();
+  };
 
-    const onProfilePress = () => {
-        navigation.navigate('Profile');
-    }
+  const onProfilePress = () => {
+    navigation.navigate('Profile');
+  };
 
   return (
     <View style={[styles.container, styles.absolutePosition]}>
-        <FeatherIcon name="arrow-left" size={30} color="black" style={styles.arrowLeft} onPress={onPressArrow}/>
+      <FeatherIcon name="arrow-left" size={30} color="black" style={styles.arrowLeft} onPress={onPressArrow} />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
         <Text style={styles.title}>Menú de opciones</Text>
-        <MenuOption icon={'account'} option={'Mi cuenta'} onPress={onProfilePress}/>
+        <MenuOption icon={'account'} option={'Mi cuenta'} onPress={onProfilePress} />
         <Separator />
         <MenuOption icon={'brightness-percent'} option={'Promociones'} />
         <Separator />
         <MenuOption icon={'sausage'} option={'Winis'} />
-        <Separator/>
+        <Separator />
         <MenuOption icon={'book-open-page-variant'} option={'Recetas'} />
         <Separator />
-        <MenuOption icon={'human-queue'} option={'Comunidades'}/>
+        <MenuOption icon={'human-queue'} option={'Comunidades'} />
         <Text style={styles.spacer}></Text>
         <MenuOption icon={'account-tie-voice'} option={'Soporte'} />
         <Separator />
         <MenuOption icon={'crosshairs-question'} option={'Sobre Nosotros'} />
+      </ScrollView>
     </View>
   );
 };
-
-export default MenuScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -65,7 +65,14 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
   },
-    spacer: {
-        height: 370,
-    },
+  spacer: {
+    height: 370,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    alignItems: 'center',
+    paddingBottom: 20,
+  },
 });
+
+export default MenuScreen;
